fbd3765a078be6655c34af33c37b89bae96cb4d2
Saurabh Rahate <saurabh-rahate@github.com>
2022-12-29 23:53:30 +0530

    RemoveDuplicatesFromSortedList2

--- src/main/java/leetcode/RemoveDuplicatesFromSortedList2.java
+++ src/main/java/leetcode/RemoveDuplicatesFromSortedList2.java
@@ -5,20 +5,39 @@ package leetcode;
 public class RemoveDuplicatesFromSortedList2 {
     public static ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) return head;
-        if (head.next.next == null)
-            if (head.val == head.next.val) return null;
-            else return head;
+        ListNode current = head;
 
-        ListNode prev = head;
-        ListNode current = head.next;
-        while (current != null && prev.val == current.val) {
-            prev = prev.next;
-            current = current.next;
-            head = current;
+        int valToRemove = Integer.MIN_VALUE;
+
+        while (current!=null) {
+            if(current.val == current.next.val) {
+                valToRemove = current.val;
+
+                while (current != null && current.val != valToRemove) {
+                    current = current.next;
+                }
+
+            }
+            if(current == head) {
+
+            }
         }
-        prev = head;
-        current = head.next;
-        ListNode next = current.next;
+//        if (head.next.next == null)
+//            if (head.val == head.next.val) return null;
+//            else return head;
+//
+//        ListNode prev = head;
+//        ListNode current = head.next;
+//        while (current != null && prev.val == current.val) {
+//            prev = prev.next;
+//            current = current.next;
+//            head = current;
+//        }
+//        prev = head;
+//        current = head.next;
+//        ListNode next = current.next;
+
+
 //        while(true) {
 //            if(current.val == next.val) {
 ////                next
