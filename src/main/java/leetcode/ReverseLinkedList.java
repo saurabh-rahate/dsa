9b5322cf080d7f2e6fc578437e3b9c00e915e651
Saurabh Rahate <saurabh-rahate@github.com>
2023-01-10 19:50:26 +0530

    Update ReverseLinkedList.java

--- src/main/java/leetcode/ReverseLinkedList.java
+++ src/main/java/leetcode/ReverseLinkedList.java
@@ -2,6 +2,11 @@ package leetcode;
 
 public class ReverseLinkedList {
 
+    public static void main(String[] args) {
+        System.out.println(reverse(ListNode.numberToList(1,2,3,4,5)));
+        System.out.println(reverse2(ListNode.numberToList(1,2,3,4,5)));
+    }
+
     public ListNode reverseList(ListNode head) {
         return reverse(head);
     }
@@ -15,6 +20,25 @@ public class ReverseLinkedList {
             head = newHead;
         }
         return head;
+    }
+
+    private static ListNode reverse2(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode prevNode = null;
+        ListNode currentNode = head;
+        ListNode nextNode = head.next;
+
+        while (nextNode != null) {
+            currentNode.next = prevNode;
+
+            prevNode = currentNode;
+            currentNode = nextNode;
+            nextNode = nextNode.next;
+        }
+        currentNode.next = prevNode;
+
+        return currentNode;
     }
 
 }
