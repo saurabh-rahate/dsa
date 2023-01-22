5b036c8ab6a8a83dc1ac1f03ec6b744d148098d6
Saurabh Rahate <saurabh-rahate@github.com>
2023-01-22 20:32:29 +0530

    Create PalindromePartitioning.java

---
+++ src/main/java/leetcode/range100_200/PalindromePartitioning.java
@@ -0,0 +1,34 @@
+package leetcode.range100_200;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        recPartition(0, s, path, res);
+        return res;
+    }
+
+    private void recPartition(int index, String s, List<String> path, List<List<String>> res) {
+        if (index == s.length()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = index; i < s.length(); i++) {
+            if (isPalindrome(s, index, i)) {
+                path.add(s.substring(index, i + 1));
+                recPartition(i + 1, s, path, res);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private static boolean isPalindrome(String s, int start, int end) {
+        while (start <= end) {
+            if (s.charAt(start++) != s.charAt(end--)) return false;
+        }
+        return true;
+    }
+}
